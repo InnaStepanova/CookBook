165fe6e1a250e5ee654b22bf93502428a7a1f489
Edgar Isaev <isaev_kdi@mail.ru>
2023-02-28 17:03:14 +0400

    Создал модуль и файды для экрана "Блюда мира" .

---
+++ CookBook/WorldDishesModule/Views/Cells/WorldDishesCollectionViewCell.swift
@@ -0,0 +1,12 @@
+//
+//  WorldDishesCollectionViewCell.swift
+//  CookBook
+//
+//  Created by Эдгар Исаев on 28.02.2023.
+//
+
+import UIKit
+
+class WorldDishesCollectionViewCell: UICollectionViewCell {
+    
+}
---
+++ CookBook/WorldDishesModule/Views/WorldDishesCollectionView.swift
@@ -0,0 +1,20 @@
+//
+//  WorldDishesCollectionView.swift
+//  CookBook
+//
+//  Created by Эдгар Исаев on 28.02.2023.
+//
+
+import UIKit
+
+class WorldDishesCollectionView: UICollectionView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
